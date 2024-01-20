05b15c1bd29cf95836e6dc3f2572aa6be4c874b4
zahra <z.aboosaeedi@gmail.com>
2024-01-20 17:15:20 +0330

    test2

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace BookMarketingVisual
         public static void Main(string[] args)
         {
 
+
             CreateHostBuilder(args).Build().Run();
         }
 
--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,6 @@ namespace BookMarketingVisual
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
